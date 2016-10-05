f8d328b178a004f9a0b669679b186a0819893c21
Genc Mazlami <genc.mazlami@uzh.ch>
2016-10-05 15:10:59 +0200

    Add ChangeEvent wrapper class.

---
+++ src/main/java/models/ChangeEvent.java
@@ -0,0 +1,48 @@
+package models;
+
+import java.util.List;
+
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+public class ChangeEvent {
+
+	private int timestampInSeconds;
+	
+	private List<DiffEntry> changedfiles;
+	
+	private RevCommit commitObject;
+
+	public int getTimestampInSeconds() {
+		return timestampInSeconds;
+	}
+
+	public void setTimestampInSeconds(int timestampInSeconds) {
+		this.timestampInSeconds = timestampInSeconds;
+	}
+
+	public List<DiffEntry> getChangedfiles() {
+		return changedfiles;
+	}
+
+	public void setChangedfiles(List<DiffEntry> changedfiles) {
+		this.changedfiles = changedfiles;
+	}
+
+	public RevCommit getCommitObject() {
+		return commitObject;
+	}
+
+	public void setCommitObject(RevCommit commitObject) {
+		this.commitObject = commitObject;
+	}
+
+	@Override
+	public String toString() {
+		return "ChangeEvent [timestampInSeconds=" + timestampInSeconds + ", changedfiles=" + changedfiles
+				+ ", commitObject=" + commitObject + "]";
+	}
+	
+	
+	
+}
