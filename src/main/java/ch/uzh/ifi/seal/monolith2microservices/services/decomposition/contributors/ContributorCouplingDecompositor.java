2b0046b243c51495764f735d2354c56efe3bb363
Genc Mazlami <genc.mazlami@uzh.ch>
2016-11-21 17:28:01 +0100

    Add NodeMapper for ContributorCoupling

---
+++ src/main/java/ch/uzh/ifi/seal/monolith2microservices/services/decomposition/contributors/ContributorCouplingDecompositor.java
@@ -0,0 +1,7 @@
+package java.ch.uzh.ifi.seal.monolith2microservices.services.decomposition.contributors;
+
+/**
+ * Created by gmazlami on 11/21/16.
+ */
+public class ContributorCouplingDecompositor {
+}
---
+++ src/main/java/ch/uzh/ifi/seal/monolith2microservices/services/decomposition/contributors/ContributorCouplingToNodeMapper.java
@@ -0,0 +1,52 @@
+package ch.uzh.ifi.seal.monolith2microservices.services.decomposition.contributors;
+
+import ch.uzh.ifi.seal.monolith2microservices.models.couplings.ContributorCoupling;
+import ch.uzh.ifi.seal.monolith2microservices.models.graph.ClassNode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Created by gmazlami on 11/21/16.
+ */
+public class ContributorCouplingToNodeMapper {
+
+    public List<ClassNode> mapToGraph(List<ContributorCoupling> couplings){
+
+        Map<String,ClassNode> nodeMap = new HashMap<>();
+
+        for(ContributorCoupling coupling: couplings){
+            String firstFileName = coupling.getFirstFileName();
+            String secondFileName = coupling.getSecondFileName();
+
+            int score = coupling.getScore();
+
+            ClassNode firstNode, secondNode;
+
+            if((firstNode = nodeMap.get(firstFileName)) == null){
+                //create node for first file in pair
+                firstNode = new ClassNode(firstFileName);
+
+            }
+            if ((secondNode = nodeMap.get(secondFileName)) == null){
+                //create node for the second file in the pair
+                secondNode = new ClassNode(secondFileName);
+            }
+
+            //link both nodes together as new neighbors with their score
+            firstNode.addNeighborWithWeight(secondNode,score);
+            secondNode.addNeighborWithWeight(firstNode,score);
+
+            nodeMap.put(firstFileName, firstNode);
+            nodeMap.put(secondFileName, secondNode);
+        }
+
+        return nodeMap.values().stream().collect(Collectors.toList());
+    }
+
+
+
+
+}
