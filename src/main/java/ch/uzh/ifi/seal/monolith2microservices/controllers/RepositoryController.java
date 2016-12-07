ecc935514406f938ccfd381733a420588603df85
Genc Mazlami <genc.mazlami@uzh.ch>
2016-12-07 16:01:16 +0100

    Refactor controllers.

--- src/main/java/ch/uzh/ifi/seal/monolith2microservices/controllers/RepositoryController.java
+++ src/main/java/ch/uzh/ifi/seal/monolith2microservices/controllers/RepositoryController.java
@@ -1,26 +1,18 @@
 package ch.uzh.ifi.seal.monolith2microservices.controllers;
 
 
+import ch.uzh.ifi.seal.monolith2microservices.dtos.RepositoryDTO;
+import ch.uzh.ifi.seal.monolith2microservices.models.git.GitRepository;
+import ch.uzh.ifi.seal.monolith2microservices.models.persistence.RepositoryRepository;
+import ch.uzh.ifi.seal.monolith2microservices.services.git.GitCloneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-
-import ch.uzh.ifi.seal.monolith2microservices.dtos.RepositoryDTO;
-import ch.uzh.ifi.seal.monolith2microservices.models.git.GitRepository;
-import ch.uzh.ifi.seal.monolith2microservices.models.persistence.RepositoryRepository;
-import ch.uzh.ifi.seal.monolith2microservices.services.decomposition.contributors.ContributorCouplingDecompositionService;
-import ch.uzh.ifi.seal.monolith2microservices.services.decomposition.semanticcoupling.SemanticCouplingDecompositionService;
-import ch.uzh.ifi.seal.monolith2microservices.services.decomposition.logicalcoupling.LogicalCouplingDecompositionService;
-import ch.uzh.ifi.seal.monolith2microservices.services.git.GitCloneService;
 
 @Configuration
 @EnableAutoConfiguration
@@ -34,15 +26,6 @@ public class RepositoryController {
 	
 	@Autowired
 	private GitCloneService gitCloneService;
-	
-	@Autowired
-	private LogicalCouplingDecompositionService logicalCouplingDecompositionService;
-
-    @Autowired
-    private ContributorCouplingDecompositionService contributorCouplingDecompositionService;
-
-	@Autowired
-	private SemanticCouplingDecompositionService semanticCouplingDecompositionService;
 
     @RequestMapping(value="/repositories", method=RequestMethod.POST)
     public GitRepository addRepository(@RequestBody RepositoryDTO repo) throws Exception{
@@ -56,26 +39,5 @@ public class RepositoryController {
     	
     	return saved;	
     }
-    
-    @RequestMapping(value="/repositories/{repoId}/decompose/logicalcoupling", method=RequestMethod.PUT)
-    public ResponseEntity<String> logicalCouplingDecomposition(@PathVariable Long repoId){
-    	GitRepository repo = repository.findById(repoId);
-    	logicalCouplingDecompositionService.process(repo);
-    	return new ResponseEntity<String>("OK", HttpStatus.OK);
-    }
-
-	@RequestMapping(value="/repositories/{repoId}/decompose/contributorCoupling", method=RequestMethod.PUT)
-	public ResponseEntity<String> contributorCouplingDecomposition(@PathVariable Long repoId){
-		GitRepository repo = repository.findById(repoId);
-		contributorCouplingDecompositionService.process(repo);
-		return new ResponseEntity<String>("OK", HttpStatus.OK);
-	}
-
-	@RequestMapping(value="/repositories/{repoId}/decompose/semanticCoupling", method=RequestMethod.PUT)
-	public ResponseEntity<String> semanticCouplingDecomposition(@PathVariable Long repoId){
-		GitRepository repo = repository.findById(repoId);
-		semanticCouplingDecompositionService.process(repo);
-		return new ResponseEntity<String>("OK", HttpStatus.OK);
-	}
     
 }
