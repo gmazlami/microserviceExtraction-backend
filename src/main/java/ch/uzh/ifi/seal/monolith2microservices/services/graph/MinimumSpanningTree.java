076bd201c56bdf98ddbc00f1af36215cc6189b59
Genc Mazlami <genc.mazlami@uzh.ch>
2016-12-08 23:53:53 +0100

    Implement MST.

--- src/main/java/ch/uzh/ifi/seal/monolith2microservices/services/graph/MinimumSpanningTree.java
+++ src/main/java/ch/uzh/ifi/seal/monolith2microservices/services/graph/MinimumSpanningTree.java
@@ -1,20 +1,48 @@
-//package ch.uzh.ifi.seal.monolith2microservices.services.graph;
-//
-//import ch.uzh.ifi.seal.monolith2microservices.models.graph.ClassNode;
-//import ch.uzh.ifi.seal.monolith2microservices.models.graph.Edge;
-//import org.jgrapht.*;
-//
-//
-///**
-// * Created by gmazlami on 12/8/16.
-// */
-//public class MinimumSpanningTree {
-//
-//
-//    private Graph<ClassNode, Edge> getGraph(){
-//
-//        Graph<ClassNode, Edge> myGraph = new SimpleWeightedGraph<ClassNode,Edge>;
-//        myGraph.addEdge(new Edge());
-//        return null;
-//    }
-//}
+package ch.uzh.ifi.seal.monolith2microservices.services.graph;
+
+import ch.uzh.ifi.seal.monolith2microservices.models.couplings.BaseCoupling;
+import org.jgrapht.alg.KruskalMinimumSpanningTree;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * Created by gmazlami on 12/8/16.
+ */
+@SuppressWarnings("unchecked")
+public class MinimumSpanningTree {
+
+    public static Set<DefaultWeightedEdge> of(List<BaseCoupling> couplings){
+        return new MinimumSpanningTree().computeMST(couplings);
+    }
+
+
+    private MinimumSpanningTree(){
+        super();
+    }
+
+
+    private Set<DefaultWeightedEdge> computeMST(List<BaseCoupling> couplings){
+        KruskalMinimumSpanningTree<String, DefaultWeightedEdge> kruskal = new KruskalMinimumSpanningTree<>(createGraph(couplings));
+        return kruskal.getMinimumSpanningTreeEdgeSet();
+    }
+
+    private SimpleWeightedGraph createGraph(List<BaseCoupling> couplings){
+        SimpleWeightedGraph<String, DefaultWeightedEdge> graph = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+
+        couplings.forEach(coupling -> {
+            graph.addVertex(coupling.getFirstFileName());
+            graph.addVertex(coupling.getSecondFileName());
+            DefaultWeightedEdge currentEdge = graph.addEdge(coupling.getFirstFileName(),coupling.getSecondFileName());
+            graph.setEdgeWeight(currentEdge, coupling.getScore());
+        });
+
+        return graph;
+    }
+
+
+
+}
