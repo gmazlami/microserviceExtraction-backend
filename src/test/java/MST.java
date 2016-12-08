8a7f885bf26831f4e96a25b9c76c3c663fe10c9f
Genc Mazlami <genc.mazlami@uzh.ch>
2016-12-08 23:12:42 +0100

    Add testcase for MST.

--- src/test/java/MST.java
+++ src/test/java/MST.java
@@ -1,9 +1,8 @@
-import ch.uzh.ifi.seal.monolith2microservices.models.graph.ClassNode;
-import ch.uzh.ifi.seal.monolith2microservices.models.graph.Edge;
-import org.jgrapht.*;
+import org.jgrapht.alg.KruskalMinimumSpanningTree;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 
 /**
@@ -15,15 +14,51 @@ public class MST {
     public void testGraph(){
         SimpleWeightedGraph<String, DefaultWeightedEdge> myGraph = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
 
-        DefaultWeightedEdge edge = new DefaultWeightedEdge();
-        ClassNode test1 = new ClassNode("test1");
-        ClassNode test2 = new ClassNode("test2");
+        myGraph.addVertex("a");
+        myGraph.addVertex("b");
+        myGraph.addVertex("c");
+        myGraph.addVertex("d");
+        myGraph.addVertex("e");
+        myGraph.addVertex("f");
+        myGraph.addVertex("g");
+        myGraph.addVertex("h");
 
-        myGraph.addVertex("test1");
-        myGraph.addVertex("test2");
-
-        DefaultWeightedEdge e1 = myGraph.addEdge("test1","test2");
+        DefaultWeightedEdge e1 = myGraph.addEdge("a","b");
         myGraph.setEdgeWeight(e1, 5);
-        System.out.println(myGraph.edgeSet());
+
+        DefaultWeightedEdge e2 = myGraph.addEdge("a","c");
+        myGraph.setEdgeWeight(e2, 2);
+
+        DefaultWeightedEdge e3 = myGraph.addEdge("b","d");
+        myGraph.setEdgeWeight(e3, 7);
+
+        DefaultWeightedEdge e4 = myGraph.addEdge("c","d");
+        myGraph.setEdgeWeight(e4, 5);
+
+        DefaultWeightedEdge e5 = myGraph.addEdge("b","e");
+        myGraph.setEdgeWeight(e5, 11);
+
+        DefaultWeightedEdge e6 = myGraph.addEdge("e","f");
+        myGraph.setEdgeWeight(e6, 1);
+
+        DefaultWeightedEdge e7 = myGraph.addEdge("f","g");
+        myGraph.setEdgeWeight(e7, 3);
+
+        DefaultWeightedEdge e8 = myGraph.addEdge("g","e");
+        myGraph.setEdgeWeight(e8, 5);
+
+        DefaultWeightedEdge e9 = myGraph.addEdge("g","h");
+        myGraph.setEdgeWeight(e9, 9);
+
+        DefaultWeightedEdge e10 = myGraph.addEdge("h","c");
+        myGraph.setEdgeWeight(e10, 4);
+
+        DefaultWeightedEdge e11 = myGraph.addEdge("d","g");
+        myGraph.setEdgeWeight(e11, 10);
+
+        KruskalMinimumSpanningTree<String, DefaultWeightedEdge> minimumSpanningTree = new KruskalMinimumSpanningTree<>(myGraph);
+
+        assert(minimumSpanningTree.getMinimumSpanningTreeEdgeSet().size() == 7d);
+        assert(minimumSpanningTree.getMinimumSpanningTreeTotalWeight() == 29d);
     }
 }
