8ef513923c6096e5c4342e5b364d5162d55c09ca
Genc Mazlami <genc.mazlami@uzh.ch>
2016-12-16 00:38:40 +0100

    Add combined decomposition service.

--- src/main/java/ch/uzh/ifi/seal/monolith2microservices/services/decomposition/DecompositionService.java
+++ src/main/java/ch/uzh/ifi/seal/monolith2microservices/services/decomposition/DecompositionService.java
@@ -1,9 +1,102 @@
 package ch.uzh.ifi.seal.monolith2microservices.services.decomposition;
 
+import ch.uzh.ifi.seal.monolith2microservices.dtos.DecompositionDTO;
+import ch.uzh.ifi.seal.monolith2microservices.graph.LinearGraphCombination;
+import ch.uzh.ifi.seal.monolith2microservices.graph.MSTGraphClusterer;
+import ch.uzh.ifi.seal.monolith2microservices.models.couplings.BaseCoupling;
+import ch.uzh.ifi.seal.monolith2microservices.models.couplings.ContributorCoupling;
+import ch.uzh.ifi.seal.monolith2microservices.models.couplings.LogicalCoupling;
+import ch.uzh.ifi.seal.monolith2microservices.models.couplings.SemanticCoupling;
+import ch.uzh.ifi.seal.monolith2microservices.models.git.ChangeEvent;
+import ch.uzh.ifi.seal.monolith2microservices.models.git.GitRepository;
+import ch.uzh.ifi.seal.monolith2microservices.models.graph.Component;
+import ch.uzh.ifi.seal.monolith2microservices.services.decomposition.contributors.ContributorCouplingEngine;
+import ch.uzh.ifi.seal.monolith2microservices.services.decomposition.logicalcoupling.LogicalCouplingEngine;
+import ch.uzh.ifi.seal.monolith2microservices.services.decomposition.semanticcoupling.SemanticCouplingEngine;
+import ch.uzh.ifi.seal.monolith2microservices.services.git.HistoryService;
+import ch.uzh.ifi.seal.monolith2microservices.services.reporting.TextFileReport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by gmazlami on 12/15/16.
  */
 public class DecompositionService {
 
+    private static final Logger logger = LoggerFactory.getLogger(DecompositionService.class);
+
+    @Autowired
+    HistoryService historyService;
+
+    @Autowired
+    SemanticCouplingEngine semanticCouplingEngine;
+
+    @Autowired
+    LogicalCouplingEngine logicalCouplingEngine;
+
+    @Autowired
+    ContributorCouplingEngine contributorCouplingEngine;
+
+    public void decompose(GitRepository repository, DecompositionDTO parameters) throws Exception{
+
+
+        List<BaseCoupling> couplings = new ArrayList<>();
+
+        if(parameters.isLogicalCoupling() && parameters.isSemanticCoupling() && parameters.isContributorCoupling()){
+
+            couplings = LinearGraphCombination.create().withLogicalCouplings(computeLogicalCouplings(repository, parameters))
+                    .withSemanticCouplings(computeSemanticCouplings(repository))
+                    .withContributorCouplings(computeContributorCouplings(repository)).generate();
+
+        }else if(parameters.isLogicalCoupling() && parameters.isSemanticCoupling()){
+
+            couplings = LinearGraphCombination.create().withLogicalCouplings(computeLogicalCouplings(repository, parameters))
+                    .withSemanticCouplings(computeSemanticCouplings(repository)).generate();
+
+        }else if(parameters.isLogicalCoupling() && parameters.isContributorCoupling()){
+
+            couplings = LinearGraphCombination.create().withLogicalCouplings(computeLogicalCouplings(repository, parameters))
+                    .withContributorCouplings(computeContributorCouplings(repository)).generate();
+
+        }else if(parameters.isContributorCoupling() && parameters.isSemanticCoupling()){
+
+            couplings = LinearGraphCombination.create().withContributorCouplings(computeContributorCouplings(repository))
+                    .withSemanticCouplings(computeSemanticCouplings(repository)).generate();
+
+        }else if(parameters.isLogicalCoupling()){
+
+            couplings = LinearGraphCombination.create().withLogicalCouplings(computeLogicalCouplings(repository, parameters)).generate();
+
+        }else if(parameters.isSemanticCoupling()){
+
+            couplings = LinearGraphCombination.create().withSemanticCouplings(computeSemanticCouplings(repository)).generate();
+
+        }else if(parameters.isContributorCoupling()){
+            couplings = LinearGraphCombination.create().withContributorCouplings(computeContributorCouplings(repository)).generate();
+        }
+
+        List<Component> components = MSTGraphClusterer.clusterFromCouplings(couplings);
+
+        TextFileReport.generate(repository, components);
+
+    }
+
+    private List<ContributorCoupling> computeContributorCouplings(GitRepository repository) throws Exception{
+        return contributorCouplingEngine.computeCouplings(historyService.computeChangeEvents(repository));
+    }
+
+    private List<SemanticCoupling> computeSemanticCouplings(GitRepository repository) throws IOException{
+        return semanticCouplingEngine.computeCouplings(repository);
+    }
+
+    private List<LogicalCoupling> computeLogicalCouplings(GitRepository repository, DecompositionDTO parameters) throws Exception{
+        List<ChangeEvent> history = historyService.computeChangeEvents(repository);
+        return logicalCouplingEngine.computeCouplings(history, parameters.getIntervalSeconds());
+    }
 
 }
