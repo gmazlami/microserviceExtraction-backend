26ddb25e7d2305d16db2f42eebec3ded4f23fa49
Genc Mazlami <genc.mazlami@uzh.ch>
2016-12-20 10:46:37 +0100

    Remove unused legacy class.

--- src/main/java/ch/uzh/ifi/seal/monolith2microservices/services/decomposition/Decompositor.java
+++
@@ -1,9 +0,0 @@
-package ch.uzh.ifi.seal.monolith2microservices.services.decomposition;
-
-import ch.uzh.ifi.seal.monolith2microservices.models.git.GitRepository;
-
-public interface Decompositor {
-	
-	void decompose(GitRepository repo);
-
-}
